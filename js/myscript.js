530d978712ef23e7b88607ebec1bebb2d8a5a606
Brian Marinai <brianmarinai.bm@gmail.com>
2020-06-12 15:41:30 +0200

    esercizio js terminato

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,11 @@
 // Chiedi all’utente il suo nome
-var nome = promt ("come ti chiami?")
-
+var nome = prompt ("come ti chiami?");
 
 // poi chiedi il suo cognome
-var cognome = promt ("quale è il tuo cognome?")
-
+var cognome = prompt ("quale è il tuo cognome?");
 
 // poi chiedi il suo colore preferito
-var colore = promt ("quale è il tuo colore preferito?")
+var colore = prompt ("quale è il tuo colore preferito?");
+
+// la tua password è......
+document.getElementById('title').innerHTML = "La tua Password è: " + nome + cognome + colore + "19"
