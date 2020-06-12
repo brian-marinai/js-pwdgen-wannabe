8f97c30d56619916c203171063f3bae1fb4b901f
Brian Marinai <brianmarinai.bm@gmail.com>
2020-06-12 15:14:58 +0200

    esercizio js inizio

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+// Chiedi all’utente il suo nome
+var nome = promt ("come ti chiami?")
+
+
+// poi chiedi il suo cognome
+var cognome = promt ("quale è il tuo cognome?")
+
+
+// poi chiedi il suo colore preferito
+var colore = promt ("quale è il tuo colore preferito?")
